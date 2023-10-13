20f74c05783d0fa8c494fb3438cba13a7486083a
Alejandro Sierro <121672722+Sierro98@users.noreply.github.com>
2023-10-13 14:46:54 +0200

    Initial Commit

---
+++ SwiftUI-Weather/SwiftUI_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_WeatherApp.swift
+//  SwiftUI-Weather
+//
+//  Created by Alejandro Sierro Galán on 13/10/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
